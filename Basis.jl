ee4f0abd066e1643220f6a06a7f416d2b30ee4a3
gustavromer <gustavromer@ymail.com>
2021-06-03 11:21:53 +0200

    Update Basis.jl

--- Basis.jl
+++ Basis.jl
@@ -1,1 +1,21 @@
 
+function unit_fourier(k)
+  sqrt_two = sqrt(2.0) 
+  if k == 0
+    return x -> 1.0
+  elseif isodd(k)
+    return x -> sqrt_two*sinpi(float(k+1)*x)
+  else
+    return x -> sqrt_two*cospi(float(k)*x)
+  end
+end
+
+function rad_fourier(k)
+  if k == 0
+    return x -> 1/ sqrt(2pi)
+  elseif isodd(k)
+    return x -> sin(float(k+1)*x / 2.) / sqrt(pi)
+  else
+    return x -> cos(float(k)*x / 2.) / sqrt(pi)
+  end
+end
