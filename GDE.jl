42f8d8d6677b169035924c7c33c3090b8867926e
gustavromer <gustavromer@ymail.com>
2021-06-03 12:05:51 +0200

    Update GDE.jl

--- GDE.jl
+++ GDE.jl
@@ -1,1 +1,6 @@
+import : Roots, Random, Distributions,BayesianNonparametricStatistics,LinearAlgebra, SparseArrays , Plots, Optim, LaTeXStrings, ProgressMeter, StatsBase
 
+include('Basis.jl')
+include('CredibleBands.jl')
+include('FixedPosterior.jl')
+include('AdaptivePosterior.jl')
