36d9ecf89bde297c02b0736c804e1927a0f9e351
gustavromer <gustavromer@ymail.com>
2021-06-16 21:02:46 +0200

    Update GenerateSec3.jl

--- Plots/GenerateSec3.jl
+++ Plots/GenerateSec3.jl
@@ -1,65 +1,61 @@
-function figure3()
-  # defining true drift
-  theta(x) = sinpi(2x) - cospi(8x)
-  # specify model (sig, x_0, T, dt) and sde
-  model = SDEModel(1.0, 0.0, 2000., 0.01)
-  sde = SDE(theta, model);
-  path = rand(sde);
-  iter = 2000;
-  basis = [fourier(k) for k in 1:50]
-  emp_pars = empBayes(path, [fourier(k) for k in 1:50]);
-  a = 25
-  b = 4;
-  # run MCMC
-  post = MCMC(path, unit_fourier, iter; j0 = 7, z0 = zeros(7), s_sq0 = emp_pars[1], alpha = emp_pars[2], A = a, B = b, C = log(0.95));
-  x = 0:0.01:1
-  f = func_from_coeffs(post[2], unit_fourier, x)[500:(iter+1),:];
-  
-  ### PLOT 1 : J | X^T ###
-  p1 = histogram(post[1][500:(iter+1)], label = "J: Posterior Samples", xticks = 7:16)
-  vline!(p1, [8.1], label = "True Number of Basis Functions", linecolor = :red, linestyle = :dash)
-  
-  ### PLOT 2  : Trace plot of s ###
-  p2 = plot(500:(iter + 1), sqrt.(post[3])[500:(iter + 1)], xlab = "Iterations", label  = "s : Posterior Samples")
-  rm = cumsum(sqrt.(post[3])) ./  collect(1.0:1.0:2001.0)
-  plot!(p2, 500:(iter + 1), rm[500:(iter + 1)], label = "Running Mean", linewidth = 1.5)
-  
-  ### PLOT 3 : Trace plot of \theta ###
-  p3 = plot()
-  plot(p3, xlab = "Iterations")
-  plot!(p3, 500:(iter + 1), f[500:(iter + 1),25], label = L"\theta(0.25)")
-  plot!(p3, 500:(iter + 1), f[500:(iter + 1),35], label = L"\theta(0.35)")
-  plot!(p3, 500:(iter + 1), f[500:(iter + 1),75], label = L"\theta(0.75)")
-  plot!(p3, 500:(iter + 1), f[500:(iter + 1),100], label = L"\theta(1.00)")
-  
-  ### PLOT 4: Estimation and credible bands ###
-  mu = vec(mean(f, dims = 1))
-  band = sim_simul_band(f, mu; p = 0.95, marg = true);
-  p4 = plot()
-  plot!(p4, x, mu, ribbon = mu .- band[1], label = "", fill = 0.25, fillcolor = :red)
-  plot!(p4, x, band[2], label = "", linecolor = :red, linestyle = :dash)
-  plot!(p4, x, mu, label = "Posterior Mean", linecolor = :black)
-  plot!(p4, x, theta, label = "True Drift", linecolor = :blue)
-  
-  ### PLOT 5: Estimation and credible bands - empBayes ###
-  emp_post = post_from_data(model, path, basis; alpha = emp_pars[2], s = emp_pars[1])
-  pars_from_emp = post_pars(emp_post, x, basis);
-  p5 = post_plot(pars_from_emp[1], pars_from_emp[2])
-  plot!(p5, x, theta, label = "True Drift", linecolor = :blue)
-  
-  ### PLOT 6: Posterior Simualtions - empBayes ###
-  p6 = plot()
-  for i in 1:10
-      plot!(p6, x, rand(emp_post), label = "", linestyle = :dash)
-  end  
-  plot!(p6, x, theta, linecolor = :blue, label = "True Drift")
-  
-  ### PLOT 7: posterior simulations - MCMC###
-  p5 = plot()
-  for i in 1:10
-    plot!(p5, x, f[1850-i,:], label = "", linestyle = :dash)
-  end    
-  plot!(p5, x, theta, linecolor = :blue, label = "True Drift")
-  
-  return (p1,p2,p3,p4,p5)
+function a(x)
+    if x > 2/3
+        return 2/7 + (2/7)x
+    else
+        return 2 / 7 - x - 2/7 * (1. - 3x)sqrt(abs(1. - 3x))
+    end
 end
+theta(x) = 12. * (a(mod(x,1.0)) + 0.05);
+N = 50; basis = [unit_fourier(k) for k in 0:(N-1)]
+x = 0:0.01:1; model = SDEModel(1.0, 0.0, 1000., 0.01); sde = SDE(theta, model);
+n_obs = 1000; ests = zeros(n_obs, 2); 
+for i in 1:n_obs path = rand(sde);ests[i,:] = empBayes(path, basis); end
+histogram(ests[:,2], bins = 20,label = "", xlab = L"\hat{\alpha}", alpha = 0.65)
+plot!([1.5], seriestype="vline",linestyle = :dash, linewidth = 1.5, label = "True Holder Exponent", legend=:topleft,linecolor = :red,size = (450, 350), dpi = 600)
+savefig("figures/sec3/fig1.png")
+scatter(ests[:,2], ests[:,1], xlab = L"\hat{\alpha}",ylab = L"\hat{s}", label = "",size = (450, 350), dpi = 600); savefig("figures/sec3/fig2.png");
+n_curves = 20; pars = [(1, 2.5), (1, 0.5), (20, 2)];
+plots = [plot(), plot(), plot(), plot()];
+for i=1:n_curves, j=1:4
+    path = rand(sde);
+    if j == 4 (s, a) = empBayes(path, basis) else (s, a) = pars[j] end
+    post = post_from_data(model, path, basis, s = s_emp, alpha = a_emp)
+    plot!(plots[j], x, rand(post), linestyle = :dash, label = "")
+    end
+end;
+for j in 1:4
+    plot(plots[j], size = (450, 300), dpi = 600, legend=:bottomright);
+    savefig("figures/sec3/fig" * string(j+3) * ".png");
+end
+path = rand(sde); emp_ests = empBayes(path, basis);
+emp_post = post_from_data(model, path, basis; alpha = emp_ests[2], s = emp_ests[1]); emp_pars = post_pars(emp_post, x, basis);
+u_fixed = fixed_band(emp_post, x; p = 0.95, N = 10^3, marg = true); u_calc = simul_band(emp_pars[2]);
+u = emp_bayes_band(mean(emp_post), basis, x, model; p = 0.95, N = 750, marg = true);
+plot(x, emp_pars[1] .+ u[2][:,1], linecolor = :red, linestyle = :dash, label = "Random Prior: Simultaneous Band")
+plot!(x, emp_pars[1] .+ u[2][:,2], linecolor = :red, linestyle = :dash, label = "")
+plot!(x, u_fixed[2][:,1], linecolor = :blue, label = "", linestyle = :dash)
+plot!(x, u_fixed[2][:,2], linecolor = :blue, label = "Fixed Prior: Simultaneous Band", linestyle = :dash)
+plot!(x, emp_pars[1], linecolor = :black, label = "Posterior Mean")
+plot!(x, theta, linecolor = :blue, label = "True Drift",size = (450, 300), dpi = 600, legend=:topright)
+savefig("figures/sec3/fig8.png")
+plot(x, emp_pars[1] .+ u[1][:,1], linecolor = :red, linestyle = :dash, label = "Random Prior: Pointwise Band")
+plot!(x, emp_pars[1] .+ u[1][:,2], linecolor = :red, linestyle = :dash, label = "")
+plot!(x, u_fixed[1][:,1], linecolor = :blue, label = "", linestyle = :dash)
+plot!(x, u_fixed[1][:,2], linecolor = :blue, label = "Fixed Prior: Pointwise Band", linestyle = :dash)
+plot!(x, emp_pars[1], linecolor = :black, label = "Posterior Mean")
+plot!(x, theta, linecolor = :blue, label = "True Drift",size = (450, 300), dpi = 600, legend=:topright)
+savefig("figures/sec3/fig9.png")
+H = check_cov(theta, emp_ests[1], emp_ests[2], x, model, sde, basis,  N = 500)
+plot(x, vec(H[1]), label = "Frequentist Coverage")
+hline!(x, [0.95], linestyle = :dash, linecolor = :black, label = "Bayesian Posterior Coverage",ylims = (0.875,1.0))
+plot!(size = (450, 300), dpi = 600, legend=:bottomleft); savefig("figures/sec3/fig10.png");
+point_plot = plot(); loop = [(1.0,2.5), (1.0, 0.5), (20.0, 2.0)]
+for i in 1:3
+    s = loop[i][1]
+    al = loop[i][2]
+    int_s = check_cov(theta, s, al, x, model, sde, basis, N = 500)
+    plot!(x, vec(int_s[1]), label = latexstring("\$ s = $(s), \\alpha = $(al) \$"))
+end
+hline!([0.95], label = "Bayesian Posterior Coverage", linestyle = :dash, linecolor = :black);
+plot(point_plot, legend = :bottomleft, ylim = (0.875,1),size = (450, 300), dpi = 600)
+savefig("figures/sec3/fig11.png")
