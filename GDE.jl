bbd4fec7382344bf175cd02cbbed52f3ab00af4e
gustavromer <gustavromer@ymail.com>
2021-06-16 21:07:17 +0200

    Delete GDE.jl

--- GDE.jl
+++
@@ -1,6 +0,0 @@
-import Roots, Random, Distributions,BayesianNonparametricStatistics,LinearAlgebra, SparseArrays , Plots, Optim, LaTeXStrings, ProgressMeter, StatsBase, DelimitedFiles
-
-include('Basis.jl')
-include('CredibleBands.jl')
-include('FixedPosterior.jl')
-include('AdaptivePosterior.jl')
